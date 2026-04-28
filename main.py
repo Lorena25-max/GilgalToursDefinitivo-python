cccb99145a130a94a6450f45697c564bdf539f9c
Daniel Tirado <Ddantir8828@gmail.com>
2026-04-28 15:49:43 -0500

    feature: limpieza de fechas y novedades, aunque eso está muy raro

--- main.py
+++ main.py
@@ -12,37 +12,37 @@ print(simular_destinos_gilgal(3)) """
 
 #------------------------------------------------------------
 #Clientes Gilgal
-import pandas as pd
+""" import pandas as pd
 from utils.clientesGilgalSimulados import simular_clientes_gilgal
 
 from notebook.generador import crear_json,crear_csv
 
 #Simulo los datos
-clientesGilgalSimulados=simular_clientes_gilgal(1000)
+clientesGilgalSimulados=simular_clientes_gilgal(10)
 
 #Convertir los datos simulados en dataframe
 clientes_ordenados=pd.DataFrame(clientesGilgalSimulados)
 
 #Convierto el dataframe en csv y en json
 crear_json(clientes_ordenados,"data/clientesGilgal.json")
-crear_csv(clientes_ordenados,"data/clientesGilgal.csv")
+crear_csv(clientes_ordenados,"data/clientesGilgal.csv") """
 
 #------------------------------------------------------------
 #Destinos Gilgal
-import pandas as pd
+""" import pandas as pd
 from utils.destinosGilgalSimulador import simular_destinos_gilgal
 
 from notebook.generador import crear_json,crear_csv
 
 #Simulo los datos
-destinosGilgalSimulados=simular_destinos_gilgal(1000)
+destinosGilgalSimulados=simular_destinos_gilgal(10)
 
 #Convertir los datos simulados en dataframe
-clientes_ordenados=pd.DataFrame(destinosGilgalSimulados)
+destinos_ordenados=pd.DataFrame(destinosGilgalSimulados)
 
 #Convierto el dataframe en csv y en json
-crear_json(clientes_ordenados,"data/destinosGilgal.json")
-crear_csv(clientes_ordenados,"data/destinosGilgal.csv")
+crear_json(destinos_ordenados,"data/destinosGilgal.json")
+crear_csv(destinos_ordenados,"data/destinosGilgal.csv") """
 
 #-------------------------------------------------------------
 #Pagos Gilgal
@@ -52,18 +52,18 @@ from utils.pagosGilgalSimulados import simular_pagos_gilgal
 from notebook.generador import crear_json,crear_csv
 
 #Simulo los datos
-pagosGilgalSimulados=simular_pagos_gilgal(1000)
+pagosGilgalSimulados=simular_pagos_gilgal(10)
 
 #Convertir los datos simulados en dataframe
 pagos_ordenados=pd.DataFrame(pagosGilgalSimulados)
 
 #Convierto el dataframe en csv y en json
-crear_json(clientes_ordenados,"data/pagosGilgal.json")
-crear_csv(clientes_ordenados,"data/pagosGilgal.csv")
+crear_json(pagos_ordenados,"data/pagosGilgal.json")
+crear_csv(pagos_ordenados,"data/pagosGilgal.csv")
 
 #-------------------------------------------------------------
 #Paquetes turísticos Gilgal
-import pandas as pd
+""" import pandas as pd
 from utils.paquetesTuristicosGilgalSimulados import simular_paquetes_turisticos_gilgal
 
 from notebook.generador import crear_json,crear_csv
@@ -76,12 +76,12 @@ paquetes_ordenados=pd.DataFrame(paquetesGilgalSimulados)
 
 #Convierto el dataframe en csv y en json
 crear_json(clientes_ordenados,"data/paquetesGilgal.json")
-crear_csv(clientes_ordenados,"data/paquetesGilgal.csv")
+crear_csv(clientes_ordenados,"data/paquetesGilgal.csv") """
 
 
 #------------------------------------------------------------
 #Reservas Gilgal
-import pandas as pd
+""" import pandas as pd
 from utils.reservasGilgalSimulados import simular_reservas_gilgal
 
 from notebook.generador import crear_json,crear_csv
@@ -94,6 +94,58 @@ reservas_ordenadas=pd.DataFrame(reservasGilgalSimulados)
 
 #Convierto el dataframe en csv y en json
 crear_json(clientes_ordenados,"data/reservasGilgal.json")
-crear_csv(clientes_ordenados,"data/reservasGilgal.csv")
+crear_csv(clientes_ordenados,"data/reservasGilgal.csv") """
+
+#---------------------------------
+#Ensayo limpieza cliente
+
+""" import pandas as pd
+
+from utils.clientesGilgalSimulados import simular_clientes_gilgal
+
+from notebook.limpiezaClientes import limpiar_datos_cliente
+
+clientesGilgalSimulados=simular_clientes_gilgal(10)
+clientes_ordenados=pd.DataFrame(clientesGilgalSimulados)
+
+clientesGilgalSimuladosLimpios=limpiar_datos_cliente(clientes_ordenados)
+print(clientesGilgalSimuladosLimpios) """
 
 
+#---------------------------------------------------
+#Ensayo limpieza destinos
+
+""" import pandas as pd
+
+from utils.destinosGilgalSimulador import simular_destinos_gilgal
+
+from notebook.limpiezaDestinos import limpiar_datos_destino
+
+destinosGilgalSimulados=simular_destinos_gilgal(10)
+destinos_ordenados=pd.DataFrame(destinosGilgalSimulados)
+
+destinosGilgalSimuladosLimpios=limpiar_datos_destino(destinos_ordenados)
+print(destinosGilgalSimuladosLimpios) """
+
+
+#--------------------------------------------------
+#Ensayo limpieza pagos
+
+import pandas as pd
+
+from utils.pagosGilgalSimulados import simular_pagos_gilgal
+from notebook.limpiezaPagos import limpiar_datos_pago
+""" from notebook.generador import crear_json, crear_csv """
+
+# Simular
+pagosGilgalSimulados = simular_pagos_gilgal(10)
+
+# DataFrame
+pagos_ordenados = pd.DataFrame(pagosGilgalSimulados)
+
+# Limpiar
+pagosGilgalSimuladosLimpios = limpiar_datos_pago(pagos_ordenados)
+
+
+# Ver resultado
+print(pagosGilgalSimuladosLimpios)
--- notebook/limpiezaPagos.py
+++ notebook/limpiezaPagos.py
@@ -12,14 +12,14 @@ def limpiar_datos_pago(data_frame_pago_sucio):
 
     #2. Limpiando los textos para controlar valores inesperados
 
-    valores_esperados_metodo_pago=["Efectivo", "PSE", "Transferencia"]
+    valores_esperados_metodo_pago=["efectivo", "pse", "transferencia"]
 
     data_frame_pago_limpio["metodo_pago"]=data_frame_pago_limpio["metodo_pago"].where(
         data_frame_pago_limpio["metodo_pago"].isin(valores_esperados_metodo_pago),
         pd.NA
     )
 
-    valores_esperados_estado_pago=["Ruiz", "Tirado", "Pérez", "Diaz", "Toro"]
+    valores_esperados_estado_pago=["ruiz", "tirado", "pérez", "diaz", "toro"]
 
     data_frame_pago_limpio["estado_pago"]=data_frame_pago_limpio["estado_pago"].where(
         data_frame_pago_limpio["estado_pago"].isin(valores_esperados_estado_pago),
@@ -29,18 +29,39 @@ def limpiar_datos_pago(data_frame_pago_sucio):
     #Limpieza de datos numéricos
 
     #1.Verificar que los números sí sean números
-    data_frame_limpio=["id_pago"]=pd.to_numeric(data_frame_limpio["id_pago"])
+    data_frame_pago_limpio["id_pago"]=pd.to_numeric(data_frame_pago_limpio["id_pago"])
 
-    data_frame_limpio=["id_reserva"]=pd.to_numeric(data_frame_limpio["id_reserva"])
+    """ data_frame_pago_limpio["id_reserva"]=pd.to_numeric(data_frame_pago_limpio["id_reserva"]) """
+    # Convertir a numérico (sin romper)
+    
+    data_frame_pago_limpio["id_reserva"] = pd.to_numeric(data_frame_pago_limpio["id_reserva"], errors="coerce")#TENGO DUDAS AQUÍ
 
-    data_frame_limpio=["monto"]=pd.to_numeric(data_frame_limpio["monto"])
+    # Eliminar los inválidos TENGO DUDAS CON ESTO
+    data_frame_pago_limpio = data_frame_pago_limpio.dropna(subset=["id_reserva"])
+
+    data_frame_pago_limpio["monto"]=pd.to_numeric(data_frame_pago_limpio["monto"])
 
     #2.Verifiquemos los valores numéricos esperados
-    data_frame_limpio=data_frame_limpio[data_frame_limpio["id_pago"]>0]
+    data_frame_pago_limpio=data_frame_pago_limpio[data_frame_pago_limpio["id_pago"]>0]
 
-    data_frame_limpio=data_frame_limpio[data_frame_limpio["id_reserva"]>0]
+    data_frame_pago_limpio=data_frame_pago_limpio[data_frame_pago_limpio["id_reserva"]>0]
 
-    data_frame_limpio=data_frame_limpio[data_frame_limpio["id_pago"]>150000]
+    data_frame_pago_limpio=data_frame_pago_limpio[data_frame_pago_limpio["id_pago"]>150000]
 
-    #Limpieza de fechas
+    #Rutina para evaluar fechas
+    #Evaluar que una fecha si es una fecha
+    data_frame_pago_limpio["fecha_pago"]=pd.to_datetime(data_frame_pago_limpio["fecha_pago"])
+
+    #Reemplazar una fecha por defecto si el campo llega vacío
+    fecha_default=pd.to_datetime("1990-06-28")
+    data_frame_pago_limpio["fecha_pago"]=data_frame_pago_limpio["fecha_pago"].fillna(fecha_default)
+
+    #Rutina para evaluar novedades
+    #Rutina para evaluar campos obligatorios que vienen vacíos
+    columnas_obligatorias=["id_pago", "id_reserva"]
+    data_frame_pago_limpio=data_frame_pago_limpio.dropna(subset=columnas_obligatorias)
+
+    data_frame_pago_limpio=data_frame_pago_limpio.drop_duplicates()
+
+    return data_frame_pago_limpio
     
