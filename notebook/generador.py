dca06ec135f27f608261699f759ff7f5ad93c3da
Daniel Tirado <Ddantir8828@gmail.com>
2026-04-29 07:38:54 -0500

    fix: comentamos dos prints que nos duplicaba las tablas

--- notebook/generador.py
+++ notebook/generador.py
@@ -2,12 +2,12 @@ import pandas as pd
 
 #Rutina para crear un archivo json desde python
 def crear_json(data_frame,nombre_archivo):
-    print(data_frame)
+    """ print(data_frame) """
     data_frame.to_json(nombre_archivo, orient="records",indent=5)
 
 
 
 #Rutina para crear un archivo csv desde python
 def crear_csv(data_frame,nombre_archivo):
-    print(data_frame)
+    """ print(data_frame) """
     data_frame.to_csv(nombre_archivo,index=False)
